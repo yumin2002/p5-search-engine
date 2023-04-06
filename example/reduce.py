bd7847f991154f1b932f101ee6fd83bb5c3d4bb2
Susie Su <76606272+SusiesuS@users.noreply.github.com>
2023-04-05 22:06:21 -0400

    changed example mapreduce

--- example/reduce.py
+++ example/reduce.py
@@ -12,13 +12,14 @@ def main():
 
 def keyfunc(line):
     """Return the key from a TAB-delimited key-value pair."""
-    return line.partition("\t")[0]
+    return line.partition("\t")[0][0]
 
 
 def reduce_one_group(key, group):
     """Reduce one group."""
     word_count = 0
     for line in group:
+        print(key, line)
         count = line.partition("\t")[2]
         word_count += int(count)
     print(f"{key} {word_count}")
